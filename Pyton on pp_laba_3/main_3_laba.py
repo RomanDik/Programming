3d78f757e2e0682d3d790f2f4fcfc1f880d3c190
Roman Dik <roman75d@yandex.ru>
2023-12-18 13:41:33 +0400

    final_3

--- Pyton on pp_laba_3/main_3_laba.py
+++ Pyton on pp_laba_3/main_3_laba.py
@@ -1,12 +1,12 @@
 import sys
 
 from part_1 import make_annotation
-from part_2 import make_annotation_2
-from part_3 import make_annotation_3
+from part_2 import make_dataset_2, make_annotation_2
+from part_3 import make_dataset_3, make_annotation_3
 from part_5 import Iterator
 
-from PyQt5.QtGui import QPainter, QPixmap
 from PyQt5.QtCore import Qt, QEvent
+from PyQt5.QtGui import QPainter, QPixmap
 from PyQt5.QtWidgets import  *
 
 
@@ -18,7 +18,7 @@ class Window(QMainWindow):
         self.initIterators()
         self.createActions()
         self.createMenuBar()
-        self.setGeometry(300, 100, 1200, 800)
+        self.setGeometry(450, 200, 1200, 800)
 
     """Главноt окно и кнопки"""
     def initUI(self) -> None:
@@ -84,11 +84,16 @@ class Window(QMainWindow):
     def createMenuBar(self) -> None:
         menuBar = self.menuBar()
 
-        self.fileMenu = menuBar.addMenu('&File')
+        self.fileMenu = menuBar.addMenu('&Menu')
         self.fileMenu.addAction(self.exitAction)
-        
+        self.fileMenu.addAction(self.changeAction)
+
         self.annotationMenu = menuBar.addMenu('&Annotation')
         self.annotationMenu.addAction(self.createannotationAction)
+
+        self.dataMenu = menuBar.addMenu('&Other Dataset')
+        self.dataMenu.addAction(self.createData2Action)
+        self.dataMenu.addAction(self.createData3Action)
 
     """Действия в меню"""
     def createActions(self) -> None:
@@ -96,14 +101,28 @@ class Window(QMainWindow):
         self.exitAction = QAction('&Exit')
         self.exitAction.triggered.connect(qApp.quit)
 
-        self.createannotationAction = QAction('&Create annotation for current dataset')
+        self.changeAction = QAction('&Change dataset')
+        self.changeAction.triggered.connect(self.changeDataset)
+
+        self.createannotationAction = QAction('&Create annotation')
         self.createannotationAction.triggered.connect(self.createAnnotation)
 
+        self.createData2Action = QAction('&dataset2')
+        self.createData2Action.triggered.connect(self.Dataset2)
+
+        self.createData3Action = QAction('&dataset3')
+        self.createData3Action.triggered.connect(self.Dataset3)
+    
+        
     """Аннотация для текущего dataset"""
     def createAnnotation(self) -> None:
     
-        make_annotation()
-        
+        if 'dataset' in str(self.folderpath):
+            make_annotation()
+        elif 'dataset_2' in str(self.folderpath):
+            make_annotation_2()
+        elif 'dataset_3' in str(self.folderpath):
+            make_annotation_3()
 
     """Действия с dataset..."""
     """Изменение текущего dataset"""
@@ -115,9 +134,18 @@ class Window(QMainWindow):
         else:
             pass
     
+    """Создание dataset_2 (имя класса и номер)"""
+    def Dataset2(self) -> None:
+        make_dataset_2()
+        
+
+    """Создание dataset_3 (рандом)"""
+    def Dataset3(self) -> None:
+        make_dataset_3()
+
     """Подтверждение"""
     def closeEvent(self, event: QEvent) -> None:
-        reply = QMessageBox.question(self, 'Warning of escaping', 'Are you sure to escape?',
+        reply = QMessageBox.question(self, 'Message', 'Are you sure to escape?',
                                      QMessageBox.Yes | QMessageBox.No)
         if reply == QMessageBox.Yes:
             event.accept()
